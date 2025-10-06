f9a20db6830e6fd5e2a85a499024ff2e1dc091ed
hafidnrzs <hafidnurazis@gmail.com>
2025-10-06 16:16:32 +0700

    Create AddUser use case and PasswordHash in the application layer

---
+++ src/Applications/use_case/_test/AddUserUseCase.test.js
@@ -0,0 +1,73 @@
+/* eslint-disable no-undef */
+import RegisterUser from "../../../Domains/users/entities/RegisterUser";
+import RegisteredUser from "../../../Domains/users/entities/RegisteredUser";
+import UserRepository from "../../../Domains/users/UserRepository";
+import PasswordHash from "../../security/PasswordHash";
+import AddUserUseCase from "../AddUserUseCase";
+
+describe("AddUserUseCase", () => {
+  it("should orchestrating the add user action correctly", async () => {
+    // Arrange
+    const useCasePayload = {
+      username: "dicoding",
+      password: "secret",
+      fullname: "Dicoding Indonesia",
+    };
+
+    const mockRegisteredUser = new RegisteredUser({
+      id: "user-123",
+      username: useCasePayload.username,
+      fullname: useCasePayload.fullname,
+    });
+
+    /** creating dependency of use case */
+    const mockUserRepository = new UserRepository();
+    const mockPasswordHash = new PasswordHash();
+
+    /** mocking needed function */
+    mockUserRepository.verifyAvailableUsername = jest
+      .fn()
+      .mockImplementation(() => Promise.resolve());
+    mockPasswordHash.hash = jest
+      .fn()
+      .mockImplementation(() => Promise.resolve("encrypted_password"));
+    mockUserRepository.addUser = jest
+      .fn()
+      .mockImplementation(() => Promise.resolve(mockRegisteredUser));
+
+    /** creating use case instance */
+    const getUserUseCase = new AddUserUseCase({
+      userRepository: mockUserRepository,
+      passwordHash: mockPasswordHash,
+    });
+
+    // Action
+    const registeredUser = await getUserUseCase.execute(useCasePayload);
+
+    // Assert
+    expect(registeredUser).toStrictEqual(
+      new RegisteredUser({
+        id: "user-123",
+        username: useCasePayload.username,
+        fullname: useCasePayload.fullname,
+      })
+    );
+    expect(mockUserRepository.verifyAvailableUsername).toHaveBeenCalledWith(
+      useCasePayload.username
+    );
+    expect(mockPasswordHash.hash).toHaveBeenCalledWith(useCasePayload.password);
+    expect(mockUserRepository.addUser).toHaveBeenCalledWith(
+      new RegisterUser({
+        username: useCasePayload.username,
+        password: "encrypted_password",
+        fullname: useCasePayload.fullname,
+      })
+    );
+  });
+});
+
+/**
+ * AddUserUseCase function is to manage the flow when doing add user
+ * action. The use case orchestrate the repository and service that
+ * being used in order the action can be successfully done.
+ */
