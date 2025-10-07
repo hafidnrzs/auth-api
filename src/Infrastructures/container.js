4640d4cbc82bdc8c68c861c779ccfb3590682e11
hafidnrzs <hafidnurazis@gmail.com>
2025-10-07 10:55:43 +0700

    Create Service Locator

---
+++ src/Infrastructures/container.js
@@ -0,0 +1,54 @@
+/* istanbuil ignore file */
+import { createContainer } from "instances-container";
+
+// external agency
+import { nanoid } from "nanoid";
+import bcrypt from "bcrypt";
+import pool from "./database/postgres/pool";
+
+// service (repository, helper, manager, etc)
+import UserRepositoryPostgres from "./repository/UserRepositoryPostgres";
+import BcryptPasswordHash from "./security/BcryptPasswordHash";
+
+// use case
+import AddUserUseCase from "../Applications/use_case/AddUserUseCase";
+import UserRepository from "../Domains/users/UserRepository";
+import PasswordHash from "../Applications/security/PasswordHash";
+
+// creating container
+const container = createContainer();
+
+// registering service and repository
+container.register([
+  {
+    key: UserRepository.name,
+    Class: UserRepositoryPostgres,
+    parameter: {
+      dependencies: [{ concrete: pool }, { concrete: nanoid }],
+    },
+  },
+  {
+    key: PasswordHash.name,
+    Class: BcryptPasswordHash,
+    parameter: {
+      dependencies: [{ concrete: bcrypt }],
+    },
+  },
+]);
+
+// registering use cases
+container.register([
+  {
+    key: AddUserUseCase.name,
+    Class: AddUserUseCase,
+    parameter: {
+      injectType: "destructuring",
+      dependencies: [
+        { name: "userRepository", internal: UserRepository.name },
+        { name: "passwordHash", internal: PasswordHash.name },
+      ],
+    },
+  },
+]);
+
+export default container;
